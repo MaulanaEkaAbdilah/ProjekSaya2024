4f28f0bfc819df3b781b12d6e341435d008fdd59
MaulanaEkaAbdilah <159239375+MaulanaEkaAbdilah@users.noreply.github.com>
2024-02-19 15:14:53 +0700

    Update Data_Mahasiswa.php
    update data kelas

--- Data_Mahasiswa.php
+++ Data_Mahasiswa.php
@@ -1,5 +1,6 @@
 <?php
-echo "<h1>Data Mahhasiswa</h1>"
-echo "Nim : 22110048 <br>"
-echo "Nama Lengkap : Maulana Eka Abdilah"
+echo "<h1>Data Mahhasiswa</h1>";
+echo "Nim : 22110048 <br>";
+echo "Nama Lengkap : Maulana Eka Abdilah";
+echo "Kelas : 22 MI";
 ?>
